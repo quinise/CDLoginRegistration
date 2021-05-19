286897f44ff700a861174548d6fc683b353918d7
Devin Ercolano <devin.ercolano@gmail.com>
2021-05-19 09:52:42 -0700

    Initial Commit

---
+++ CDLoginRegistration/CDLoginRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDLoginRegistrationApp.swift
+//  CDLoginRegistration
+//
+//  Created by Devin Ercolano on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CDLoginRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
